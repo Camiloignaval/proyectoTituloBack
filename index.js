d44c5847530fb5772a3b6f5c11f47984b7579793
Camiloignaval <camiloignaval@gmail.com>
2022-07-07 22:40:45 -0400

    fix

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-const express = require('express')
-const cors = require('cors')
-const dayjs = require('dayjs')
-require('dotenv').config()
-require('dayjs/locale/es')
-dayjs.locale('es')
+const express = require("express");
+const cors = require("cors");
+const dayjs = require("dayjs");
+require("dotenv").config();
+require("dayjs/locale/es");
+dayjs.locale("es");
 
-const app = express()
+const app = express();
 // cors
-app.use(cors())
+app.use(cors());
 
 // lectura y parseo de body
-app.use(express.json())
+app.use(express.json());
 
 // directorio publico
-app.use(express.static('public'))
+app.use(express.static("public"));
 
 // Rutas
-app.use('/api/auth', require('./routes/auth'))
-app.use('/api/admin', require('./routes/admin'))
-app.use('/api/msg', require('./routes/msg'))
-app.use('/api/user', require('./routes/user'))
-app.use('/api/personal', require('./routes/personal'))
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/admin", require("./routes/admin"));
+app.use("/api/msg", require("./routes/msg"));
+app.use("/api/user", require("./routes/user"));
+app.use("/api/personal", require("./routes/personal"));
 
 // escuchar peticiones
-app.listen(process.env.PORT, () => {
-  console.log(`Escuchando puerto ${process.env.PORT}`)
-})
+app.listen(process.env.PORT ?? 5000, () => {
+  console.log(`Escuchando puerto ${process.env.PORT}`);
+});
